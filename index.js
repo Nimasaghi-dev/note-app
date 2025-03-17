7bd2d33fc769e6b12f24e586ecb579aeb2270910
nima <nimasaghi.dev@gmail.com>
2025-03-17 18:31:22 +0100

    feat: Add dynamic note deletion and real-time storage updates

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const notesContainer = document.querySelector('.notes-container');
 const createBtn = document.querySelector('.btn');
 let notes = document.querySelectorAll('.input-box');
 
+function ShowNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+ShowNotes();
+
+function updateStorage () {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener('click', () => {
     let inputBox = document.createElement('p');
     let img = document.createElement('img');
@@ -14,5 +23,13 @@ createBtn.addEventListener('click', () => {
 notesContainer.addEventListener('click', (e) => {
     if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
+        updateStorage ();
+    } else if (e.target.tagName === "p") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+            }
+        })
     }
 })
